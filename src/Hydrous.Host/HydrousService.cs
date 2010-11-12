8aa0737e78306cf731ad9dddb632d69b3e125811
Darren Kopp <darrenkopp@gmail.com>
2010-11-11 21:34:52 -0700

    Added HydrousService for when host is running as installed windows service.

---
+++ src/Hydrous.Host/HydrousService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceProcess;
+
+namespace Hydrous.Host
+{
+    public class HydrousService : ServiceBase
+    {
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+        protected override void OnShutdown()
+        {
+            base.OnShutdown();
+        }
+    }
+}
--- src/Hydrous.Host/Program.cs
+++ src/Hydrous.Host/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceProcess;
 
 namespace Hydrous.Host
 {
@@ -9,6 +10,22 @@ namespace Hydrous.Host
     {
         static void Main(string[] args)
         {
+            if (Environment.UserInteractive)
+            {
+                RunInteractive(args);
+            }
+            else
+            {
+                // if we aren't interactive, then we should run as a service
+                ServiceBase.Run(new HydrousService());
+            }
+        }
+
+        private static void RunInteractive(string[] args)
+        {
+            // TODO: run with interactive console
+            // TODO: allow commands like stop [service], restart, start, etc
+            throw new NotImplementedException();
         }
     }
 }
