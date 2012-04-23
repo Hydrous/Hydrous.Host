8030c75659dbb967c9ced00607cf6307c9493808
Darren Kopp <darrenkopp@gmail.com>
2012-04-22 19:43:22 -0600

    Added build script

---
+++ buildscripts/default.ps1
@@ -0,0 +1,51 @@
+properties {
+    $assemblyVersion = "1.0.0.0"
+    $fileVersion = "1.0.0.0"
+    $basePath = resolve-path ..\
+    $tools = "$basePath\tools"
+    $build = "$basePath\build"
+    $release = "$basePath\release"
+}
+
+task default
+
+task Clean {
+    rmdir $build -recurse -force -ErrorAction SilentlyContinue
+    rmdir $release -recurse -force -ErrorAction SilentlyContinue
+    
+    mkdir $build | out-null
+    mkdir $release | out-null
+}
+
+task Version {
+    
+}
+
+task Build {
+    exec { `
+        msbuild /m "$basePath\src\Hydrous.Host.sln" `
+            /v:m `
+            /t:Build `
+            /p:Configuration=Release `
+            /p:OutputPath="$build"
+    }
+}
+
+task Zip {
+    [System.Reflection.Assembly]::LoadFrom("$tools\DotNetZip\Ionic.Zip.dll") | out-null;
+
+    $zipfile =  new-object Ionic.Zip.ZipFile;
+    $e= $zipfile.AddFile("$build\Hydrous.Host.exe","");
+    $e = $zipfile.AddFile("$build\Hydrous.Host.pdb","");
+    $e = $zipfile.AddFile("$build\Hydrous.Hosting.Core.dll","");
+    $e = $zipfile.AddFile("$build\Hydrous.Hosting.Core.pdb","");
+    $e = $zipfile.AddFile("$build\Hydrous.Hosting.dll","");
+    $e = $zipfile.AddFile("$build\Hydrous.Hosting.pdb","");
+    $e = $zipfile.AddFile("$build\log4net.config","");
+    $zipfile.Save("$release\$fileVersion.zip");
+    $zipfile.Dispose();
+}
+
+task Release -depends Clean, Version, Build, Zip {
+    
+}
